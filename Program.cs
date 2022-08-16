7a70330d4ab611460861fcae03e9c16d5e00d2fe
yasin <yasinsivis9@gmail.com>
2022-08-16 11:20:44 +0300

    Dosyalar Yüklendi

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+ArrayList Tutucu=new ArrayList();
+int first=0;
+int even=0;
+Console.WriteLine("Lütfen Birinci Pozitif Tam Sayıyı Giriniz:");
+first=Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Lütfen İkinci Pozitif Tam Sayıyı Giriniz:");
+even=Convert.ToInt16(Console.ReadLine());
+int ConserVative = 0;
+for(int i=1; i<=first; i++)
+{
+    Console.WriteLine("Lütfen"+" "+i+" "+".Sayıyı Giriniz:");
+    ConserVative=Convert.ToInt16(Console.ReadLine());
+
+    if(ConserVative==even || ConserVative%even==0)
+    {
+        Tutucu.Add(ConserVative);
+    } 
+    else if(even % ConserVative==0)  Tutucu.Add(ConserVative);
+
+}
+Console.WriteLine("_________________----------_______________");
+
+foreach (var sayi in Tutucu)
+{
+    Console.WriteLine("Saıyıa Eşit Olan Veya Tam Bölünen Sayılar"+" "+sayi);
+}
